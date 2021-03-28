7c56396a903893c5d4f421cb46eaeb5934146cab
K_on_Mac <gemigpost@hotmail.com>
2021-03-28 19:33:59 +0200

    Final Commit

--- Application/Profiles/ListActivities.cs
+++ Application/Profiles/ListActivities.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
-using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
